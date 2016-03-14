43642166fbdeff392f8911a0c20cf41359c69342
Matthew Phelps <mdp553@hotmail.com>
2016-03-14 11:44:50 +0100

    Add normalization to main df

--- data-raw/dk-cholera-daily.R
+++ data-raw/dk-cholera-daily.R
@@ -7,7 +7,7 @@
 rm(list = ls())
 
 
-aalbrog_daily_data <- read.csv('data-raw\\Aalborg_daily_1853.csv')
+aalborg_daily_data <- read.csv('data-raw\\Aalborg_daily_1853.csv')
 cph_daily_data <- read.csv('data-raw\\CPH_daily_1853.csv')
 korsoer_daily_data <- read.csv('data-raw\\Korsoer_daily_1857.csv')
 brandholm_daily_data <- read.csv("data-raw\\Brandhom_daily_1850.csv")
@@ -16,16 +16,17 @@ brandholm_daily_data <- read.csv("data-raw\\Brandhom_daily_1850.csv")
 aalborg_pop <- 7745
 cph_pop <- 143591
 korsoer_pop <- 2236
+brandholm_pop <- 700
 
 
 
 # DATE PREPERATION --------------------------------------------------------
 
-aalbrog_daily_data$month <- sprintf("%02d", aalbrog_daily_data$month) # pad with leading zeros
-aalbrog_daily_data$date <- paste(aalbrog_daily_data$years, aalbrog_daily_data$month, aalbrog_daily_data$day, sep = "-")
-aalbrog_daily_data$date <- as.Date(aalbrog_daily_data$date)
-aalbrog_daily_data$city <- "aalborg"
-aalbrog_daily_data$month <- as.integer(aalbrog_daily_data$month)
+aalborg_daily_data$month <- sprintf("%02d", aalborg_daily_data$month) # pad with leading zeros
+aalborg_daily_data$date <- paste(aalborg_daily_data$years, aalborg_daily_data$month, aalborg_daily_data$day, sep = "-")
+aalborg_daily_data$date <- as.Date(aalborg_daily_data$date)
+aalborg_daily_data$city <- "aalborg"
+aalborg_daily_data$month <- as.integer(aalborg_daily_data$month)
 
 
 
@@ -51,10 +52,27 @@ brandholm_daily_data$month <-as.integer(brandholm_daily_data$month)
 
 
 
+# NORMALIZING -------------------------------------------------------------
+
+aalborg_daily_data$cases_norm <- aalborg_daily_data$cases/aalborg_pop * 10000
+aalborg_daily_data$deaths_norm <- aalborg_daily_data$deaths/aalborg_pop * 10000
+aalborg_daily_data$day_norm <- aalborg_daily_data$day_index / max(aalborg_daily_data$day_index)
+
+cph_daily_data$cases_norm <- cph_daily_data$cases/cph_pop * 10000
+cph_daily_data$deaths_norm <- cph_daily_data$deaths/cph_pop * 10000
+cph_daily_data$day_norm <- cph_daily_data$day_index / max(cph_daily_data$day_index)
+
+korsoer_daily_data$cases_norm <- korsoer_daily_data$cases/korsoer_pop * 10000
+korsoer_daily_data$deaths_norm <- korsoer_daily_data$deaths/korsoer_pop * 10000
+korsoer_daily_data$day_norm <- korsoer_daily_data$day_index / max(korsoer_daily_data$day_index)
+
+brandholm_daily_data$cases_norm <- brandholm_daily_data$cases / brandholm_pop * 10000
+brandholm_daily_data$deaths_norm <- brandholm_daily_data$deaths / brandholm_pop * 10000
+brandholm_daily_data$day_norm <- brandholm_daily_data$day_index / max(brandholm_daily_data$day_index)
 # BIND TO LONG DATASET ----------------------------------------------------
 
-cholera_daily_data <- rbind(aalbrog_daily_data, brandholm_daily_data, cph_daily_data, korsoer_daily_data)
+cholera_daily_data <- rbind(aalborg_daily_data, brandholm_daily_data, cph_daily_data, korsoer_daily_data)
 
 # SAVE --------------------------------------------------------------------
-devtools::use_data(cholera_daily_data)
+devtools::use_data(cholera_daily_data, overwrite = T)
 
