1853552383565b412010325348bc57c6c4024d00
Matthew Phelps <mdp553@hotmail.com>
2016-03-16 15:34:13 +0100

    Add compiled hospital admissions data and descriptions

---
+++ R/cholera_daily_data.R
@@ -0,0 +1,34 @@
+#' Daily incidence of cholera, 19th century Denmark.
+#'
+#' Data from all known sources that record daily morbidity and mortality due to
+#' cholera from 19th century Denmark. Cholera epidemics occurred in three years
+#' in Denmark, 1850, 1853 and 1857. Both 1850 and 1857 were isolated outbreaks
+#' in one location. In 1853 cholera was widespread in towns across the nation.
+#' For all locations daily morbidity and mortality counts were recorded.
+#'
+#'
+#' @format A data frame with 273 rows and 11 variables:
+#' \describe{
+#'   \item{\code{\bold{years}}}{Integer vector of year values}
+#'   \item{\code{\bold{month}}}{Integer vector of month values}
+#'   \item{\code{\bold{day}}}{Integer vector of day of month values}
+#'   \item{\code{\bold{day_index}}}{Integer vector of the day number, as indexed
+#'   from the beginig of the epidemic}
+#'   \item{\code{\bold{cases}}}{Integer vector of Number of people recorded with
+#'   cholera}
+#'    \item{\code{\bold{deaths}}}{Integer vector recording, of those that were
+#'    sick from cholera,    this is the number that died}
+#'     \item{\code{\bold{date}}}{Vector of date values}
+#'      \item{\code{\bold{city}}}{Name of town or city}
+#'      \item{\code{\bold{case_norm}}}{Numeric vector of the normalized case
+#'      incidence rate, per 10,000 people}
+#'      \item{\code{\bold{deaths_norm}}}{Numeric vector of the normalized death
+#'      rate due to cholera, per 10,000 people}
+#'      \item{\code{\bold{day_norm}}}{Numeric vector of the day index number,
+#'      normalized over the total length of the epidemic in that locaiton. This
+#'      number shows what percentage of total time of the epidemic has already
+#'      passed}
+#'
+#' }
+#' @source {Various}
+"cholera_daily_data"
---
+++ R/hospital_records.R
@@ -0,0 +1,19 @@
+#' Cholera hospital admissions records Denmark, 1853.
+#'
+#' A dataset of the hospital admission due to cholera during outbreaks around
+#' Denmark in 1853
+#'
+#'
+#' @format A data frame with 254 rows and 8 variables:
+#' \describe{
+#'   \item{\code{\bold{admission_order}}}{Vector of integer values describing the order in which cases were admitted to hospital }
+#'   \item{\code{\bold{date_admission}}}{Vector of date values recording the date that the patient was admitted to the hospital}
+#'   \item{\code{\bold{age}}}{Numeric vector of age of cases at admission}
+#'   \item{\code{\bold{dicharge_date}}}{Vector of dates of discharge from the hospital, presumably recovered. NA values mean either patient died or date was not recorded}
+#'   \item{\code{\bold{death_date}}}{Vector of dates of death of patient, if applicable. NA values mean either patient was discharged or date was not recorded}
+#'    \item{\code{\bold{outcome}}}{Integer vector of possible patient outcomes. 1 = death & 0 = discharged}
+#'     \item{\code{\bold{event}}}{Vector of dates on which the outcome occured. I.e., the date the patient either died or was discharged}
+#'      \item{\code{\bold{location}}}{Location of hospital - usually the town name}
+#' }
+#' @source {Various handwritten archival records from local doctors}
+"hospital_records"
