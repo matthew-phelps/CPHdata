cfe998c3b28de54fe558309f99a1d840da678272
Matthew Phelps <mdp553@hotmail.com>
2016-06-13 15:19:59 +0200

    Edit desc for cases in small towns
    This may or may not be hospital data. Checking now with Mads to see
    which it is. Will update this when Mads responds with information.0.

--- R/case_records.R
+++ R/case_records.R
@@ -1,16 +1,17 @@
 #' Cholera case records Denmark, 1853.
 #'
-#' A dataset of the hospital admission due to cholera during outbreaks around
-#' Denmark in 1853
+#' A dataset of cholera cases from two small Danish towns of Nykoebing and
+#' Frederikshavn in 1853
 #'
 #'
 #' @format A data frame with 254 rows and 8 variables:
 #' \describe{
 #'   \item{\code{\bold{admission_order}}}{Vector of integer values describing
-#'   the order in which cases were admitted to hospital }
+#'   the order in which cases were recorded - but does not seemingly correspond
+#'   to any real-life value}
 #'   \item{\code{\bold{date_admission}}}{Vector of date values recording the
-#'   date that the patient was admitted to the hospital}
-#'   \item{\code{\bold{age}}}{Numeric vector of age of cases at admission}
+#'   date that the patient was diagnosed}
+#'   \item{\code{\bold{age}}}{Numeric vector of age of cases}
 #'   \item{\code{\bold{dicharge_date}}}{Vector of dates of discharge from the
 #'   hospital, presumably recovered. NA values mean either patient died or date
 #'   was not recorded}
